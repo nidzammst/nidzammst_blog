aac37fe4729bf78e2e8bd2425b0ae6626ac440a6
Ahmad Nidzam Musthafa <nidzam0501@gmail.com>
2024-05-07 11:27:45 +0700

    ubah dikit

--- pages/api/robots/index.js
+++ pages/api/robots/index.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   const textSiteMap =
-    'User-agent: *\nAllow: *\n# Host\nHost: https://mstblog.my.id\n\nSitemap: https://mstblog.my.id/sitemap.xml'
+    'User-agent: *\nAllow: *\n# Host\nHost: https://nidzammst.my.id\n\nSitemap: https://nidzammst.my.id/sitemap.xml'
   res.send(textSiteMap)
 }
