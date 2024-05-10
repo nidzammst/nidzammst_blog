8a21165eee8f070668656f0f8e7088044de00c42
Ahmad Nidzam Musthafha <54913708+nidzammst@users.noreply.github.com>
2024-05-10 23:30:16 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import BLOG from '@/blog.config'
 import CommonScript from '@/components/CommonScript'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -15,6 +16,17 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' href='/favicon.ico' />
           <CommonScript />
+  <Script type="text/javascript">
+  atOptions = {
+    'key': '5ee41ec68d55eb28c246664ae37e62d4',
+    'format': 'iframe',
+    'height': 600,
+    'width': 160,
+    'params': {}
+  };
+/>
+<Script type="text/javascript" src="//www.topcreativeformat.com/5ee41ec68d55eb28c246664ae37e62d4/invoke.js">
+/>
         </Head>
 
         <body className={`${BLOG.FONT_STYLE} tracking-wider bg-day dark:bg-night`}>
