a6862ec08530a3c02a0811f87e1629eccb13b010
Ahmad Nidzam Musthafha <54913708+nidzammst@users.noreply.github.com>
2024-05-12 11:03:44 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,11 +17,11 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <CommonScript />
           <Script
-            src="//pl23292371.highcpmgate.com/c9/dd/b8/c9ddb866b1055a1cd466384034590267.js"
-            strategy="beforeInteractive"
-          />
-          {/* <Script
             src="//pl23292382.highcpmgate.com/90/42/a7/9042a704693cdf40f23d6e6b5ded2b1c.js"
+            strategy="beforeInteractive"
+          /> 
+          {/* <Script
+            src="//pl23292371.highcpmgate.com/c9/dd/b8/c9ddb866b1055a1cd466384034590267.js"
             strategy="beforeInteractive"
           /> */}
           {/* Popunder */}
